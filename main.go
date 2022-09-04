fce8f63e58ff021931b15ea6ab5169f518b8c1f5
Rijk van Zanten <rijkvanzanten@me.com>
2022-09-04 15:30:06 -0400

    Add JSON data loading (#13)
    Closes #1

--- main.go
+++ main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
 
+type Data []struct {
+	Movie string `json:"movie"`
+	Year int16 `json:"year"`
+	ReleaseDate string `json:"release_date"`
+	Director string `json:"director"`
+	Character string `json:"character"`
+	MovieDuration string `json:"movie_duration"`
+	Timestamp string `json:"timestamp"`
+	FullLine string `json:"full_line"`
+	CurrentWowInMovie uint8 `json:"current_wow_in_movie"`
+	TotalWowsInMovie uint8 `json:"total_wows_in_movie"`
+	Poster string `json:"poster"`
+	Video struct {
+		Res1080p string `json:"1080p"`
+		Res720p string `json:"720p"`
+		Res480p string `json:"480p"`
+		Res360p string `json:"360p"`
+	} `json:"video"`
+	Audio string `json:"audio"`
+}
+
+func main() {
+	content, err := ioutil.ReadFile("./data.json")
+
+	if err != nil {
+		log.Fatal("Error when opening file: ", err)
+	}
+
+	var wows Data
+
+	err = json.Unmarshal(content, &wows)
+
+	if err != nil {
+		log.Fatal("Error during Unmarshal(): ", err)
+	}
+
+	log.Printf("%v", wows)
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,14 @@
 # (G)Owen Wilson WOW API
 
 A Go port of the great [Owen Wilson Wow API](https://owen-wilson-wow-api.herokuapp.com). Just a way for me to practice some Go :-)
+
+## References
+
+- https://golangdocs.com/golang-read-json-file
+- https://www.digitalocean.com/community/tutorials/how-to-use-json-in-go
+- https://www.digitalocean.com/community/tutorials/how-to-run-multiple-functions-concurrently-in-go
+- https://www.digitalocean.com/community/tutorials/how-to-use-contexts-in-go
+- https://www.digitalocean.com/community/tutorials/how-to-make-an-http-server-in-go
+- https://winterflower.github.io/2017/08/20/the-asterisk-and-the-ampersand/
+- CodeCademy's Go intro course
+- https://blog.logrocket.com/creating-a-web-server-with-golang/
